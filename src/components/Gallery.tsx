0aa55e1d95ff8ed1e02674a04cb6d1c1a53efa86
boltgthb <yyolyyoyltototititi@gmail.com>
2025-11-26 20:07:09 +0300

    Updated Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -9,7 +9,7 @@ const Gallery = () => {
   const [loadingImages, setLoadingImages] = useState<{ [key: number]: boolean }>({});
 
   const galleryItems = [
-    { id: 1, caption: 'Köy Manzarası', images: ['/mahmatli-koyu-fotograf.jpg', '/mahmatli-koy-manzarasi.JPG', '/kelkit-mahmatli-koyu-foto.JPG', '/kelkit-mahmatli-koyu-panaroma.jpg' ] },
+    { id: 1, caption: 'Köy Manzarası', images: ['/mahmatli-koyu-fotograf.jpg', '/mahmatli-koy-manzarasi.JPG', '/kelkit-mahmatli-koyu-foto.JPG', '/kelkit-mahmatli-koyu-genis-aci.jpg' ] },
     { id: 2, caption: 'Kışın Mahmatlı', images: ['/kisin-mahmatli-min.JPG', '/kisin-mahmatli-manzarasi.JPG'] },
     { id: 3, caption: 'Dedenin Çayırı 4. Köprü', images: ['/mahmatli-koyu-fotograflari.JPG'] },
     { id: 4, caption: 'Harmanda Tek Kale Futbol', images: ['/kelkit-mahmatli-koyu.JPG'] },
