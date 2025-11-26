804f26b46a8ead27f87bd49be44c557258d8f6a1
boltgthb <yyolyyoyltototititi@gmail.com>
2025-11-26 18:32:10 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,10 +13,10 @@ const Hero = () => {
       id="anasayfa"
       className="relative min-h-screen flex items-center justify-center pt-16 bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: `url('/anasayfa-resim.jpeg')`
+        backgroundImage: `url('/mahmatli-koyu.JPG')`
       }}
     >
-      <div className="absolute inset-0 bg-white/50 "></div>
+      <div className="absolute inset-0 bg-white/10 "></div>
 
       <div className="relative max-w-2xl mx-auto px-4 sm:px-6 text-center">
         <div className="inline-flex items-center justify-center w-20 h-20 sm:w-24 sm:h-24 bg-primary-400 rounded-full mb-6 sm:mb-8">
